a53e92e7c83e1f9f517dcf7454906ee0447816b8
AlanEPrestia <alaneprestia@gmail.com>
2025-06-09 11:29:44 -0300

    Se agregan los templates con variables

--- core/views.py
+++ core/views.py
@@ -19,3 +19,22 @@ def saludar_con_parametros(request, nombre: str, apellido: str): # 3° ESTO
 	nombre = nombre.capitalize()
 	apellido = apellido.upper()
 	return HttpResponse(f'{apellido}, {nombre}')
+
+### QUINTO
+
+def probando_template(request):
+    contexto = {
+        "nombre": "Nicolas",
+        "apellido": "Catalano",
+        "dia": datetime.now().date(),
+        "notas": [10, 2, 4, 7, 3], #### SE MUESTRA EN EL SEGUNDO EJEMPLO
+        "notas_malas": [6, 4, 2], #### SE MUESTRA EN EL SEGUNDO EJEMPLO 
+    }
+    return render(request, "core/template1.html", contexto)
+"""
+🔍 ¿Qué hace render()?
+La función render() de Django hace tres cosas a la vez:
+Carga la plantilla HTML que le pasás.
+Rellena la plantilla con los datos del contexto (el diccionario con tus variables).
+Devuelve un objeto HttpResponse con el HTML generado como contenido.
+"""
