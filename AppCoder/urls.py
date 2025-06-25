7cd3fcc0aa6b47034148d17d9b88edae1a127351
AlanEPrestia <alaneprestia@gmail.com>
2025-06-24 23:59:25 -0300

    Cambios (clase 23/06)

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import cursos, profesores, estudiantes, entregables, inicio 
+from .views import cursos, profesores, estudiantes, entregables, inicio, cursoFormulario, cursoFormulario2,profesorFormulario,busquedaCamada, buscar
 
 urlpatterns = [
     # path('estudiantes/', lista_estudiantes, name='lista_estudiantes'),
@@ -9,4 +9,12 @@ urlpatterns = [
     path('profesores/', profesores, name='profesores'),
     path('estudiantes/', estudiantes, name='estudiantes'),
     path('entregables/', entregables, name='entregables'),
+
+    path('cursoFormulario',cursoFormulario, name='cursoFormulario'),
+    path('cursoFormulario2',cursoFormulario2, name='cursoFormulario2'),
+    path('profesorFormulario', profesorFormulario, name="profesorFormulario"),
+
+    path('busquedaCamada/', busquedaCamada, name="busquedaCamada"),
+    path('buscar/', buscar, name='buscar'),
+
 ]
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Estudiante, Profesor, Curso, Entregable
+from .forms import CursoFormulario, ProfesorFormulario
 
 # def lista_estudiantes(request):
 #     estudiantes = Estudiante.objects.all()
@@ -24,3 +26,58 @@ def estudiantes(request):
 
 def entregables(request):
     return render(request, "AppCoder/entregables.html")
+
+def cursoFormulario(request):
+      return render(request, "AppCoder/formulario/cursoFormulario.html")
+
+def cursoFormulario2(request):
+      if request.method == "POST":
+            miFormulario = CursoFormulario(request.POST) # Aqui me llega la informacion del html
+            if miFormulario.is_valid():
+                  informacion = miFormulario.cleaned_data
+                  curso = Curso(nombre=informacion["curso"], camada=informacion["camada"])
+                  curso.save()
+                  return render(request, "AppCoder/cursos.html")
+      else:
+            miFormulario = CursoFormulario() # Formulario vacio para construir el html
+ 
+      return render(request, "AppCoder/formulario/cursoFormulario2.html", {"miFormulario": miFormulario})
+
+def profesorFormulario(request):
+
+    if request.method == 'POST':
+        miFormulario = ProfesorFormulario(request.POST)  # aquí llega toda la información del html
+        if miFormulario.is_valid():  # Si pasó la validación de Django
+            informacion = miFormulario.cleaned_data
+            profesor = Profesor(
+                nombre=informacion['nombre'],
+                apellido=informacion['apellido'],
+                email=informacion['email'],
+                profesion=informacion['profesion']
+            )
+            profesor.save()
+            return render(request, "AppCoder/index.html")  # Vuelvo al inicio o a donde quieran
+    else:
+        miFormulario = ProfesorFormulario()  # Formulario vacío para construir el html
+
+    return render(request, "AppCoder/formulario/profesorFormulario.html", {"miFormulario": miFormulario})
+
+def busquedaCamada(request):
+    return render(request, "AppCoder/formulario/busquedaCamada.html")
+
+
+def buscar(request):
+    if request.GET["camada"]:
+        #respuesta = f"Estoy buscando la camada nro: {request.GET['camada'] }"
+        camada = request.GET['camada']
+        # icontains es un filtro que se usa para buscar coincidencias en los campos de texto de la base de datos, 
+        # sin importar si las letras están en mayúsculas o minúsculas
+        cursos = Curso.objects.filter(camada__icontains=camada)
+
+        return render(request, "AppCoder/formulario/resultadosBusqueda.html", {"cursos": cursos, "camada": camada})
+
+    else:
+        respuesta = "No enviaste datos"
+
+        # No olvidar from django.http import HttpResponse
+        return HttpResponse(respuesta)
